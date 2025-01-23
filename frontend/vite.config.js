bd1d6f8d11826e89b9ab54667b7ce09e22e81396
EmericHrv <emeric.hrv@gmail.com>
2025-01-23 15:26:00 +0100

    Enhance Vite configuration with build output structure and server settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,21 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.js', // Facultatif si `postcss.config.js` est bien configuré
+    postcss: './postcss.config.js', // Spécifiez PostCSS si nécessaire
+  },
+  build: {
+    outDir: 'dist', // Répertoire de sortie pour les fichiers de production
+    rollupOptions: {
+      output: {
+        // Structure des fichiers générés
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+      },
+    },
+  },
+  server: {
+    host: true, // Permet d'accéder au serveur sur le réseau local
+    port: 3000, // Définit le port par défaut pour le développement
   },
 });

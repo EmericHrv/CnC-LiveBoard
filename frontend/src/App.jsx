4faa4d50597bb811f53c9e63f435dec427334c0e
EmericHrv <emeric.hrv@gmail.com>
2025-01-25 12:38:18 +0100

    Add InfosPage import to App component for routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import LoginPage from './pages/LoginPage/LoginPage';
 import DashboardPage from './pages/DashboardPage/DashboardPage';
 import MainPage from './pages/MainPage/MainPage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import InfosPage from '../InfosPage/InfosPage';
 
 // Route privée : nécessite un token valide
 const PrivateRoute = ({ children }) => {
